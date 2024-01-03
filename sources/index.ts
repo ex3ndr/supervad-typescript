9a61acae47ca2144b25eb1049b4cb36eb637064a
Steve Korshakov <steve@korshakov.com>
2024-01-02 23:24:13 -0800

    fix: missing event class

--- sources/index.ts
+++ sources/index.ts
@@ -1,3 +1,3 @@
 export { SuperVADEngine } from './engine';
 export { SuperVADParameters, optimalParameters } from './settings';
-export { SuperVADRealtime, SuperVADRealtimeState } from './realtime';
+export { SuperVADRealtime, SuperVADRealtimeState, SuperVADRealtimeEvent } from './realtime';
